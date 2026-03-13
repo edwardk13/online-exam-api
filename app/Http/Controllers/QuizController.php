af2dbab5bef3d999416b7fcfb70743c95dbcd48a
edwardk13 <khiemlmaopro@gmail.com>
2026-03-13 13:00:29 +0700

    init

---
+++ app/Http/Controllers/QuizController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    // Logic for retrieving quiz questions
+    public function getQuiz()
+    {
+        //
+    }
+
+    // Logic for submitting and grading the quiz
+    public function submitQuiz(Request $request)
+    {
+        //
+    }
+}
